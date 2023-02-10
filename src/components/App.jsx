1956efc077e639a0d74c7bc4c5f6e870743e186d
Grégory Marini <greg.marini@hotmail.fr>
2023-02-10 21:46:08 +0100

    add createArea component and update style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { notes } from "../notes";
+import CreateArea from "./CreateArea";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
@@ -8,6 +9,7 @@ const App = () => {
     return (
         <div>
             <Header />
+            <CreateArea />
             {notes.map((note) => (
                 <Note
                     key={note.key}
